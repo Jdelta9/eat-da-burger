debd46469f0e7caab7811592c598174003835a9b
Jdelta9 <35478858+Jdelta9@users.noreply.github.com>
2018-06-09 00:30:37 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,11 @@ var mysql = require('mysql');
 // Create the MySQL connection object
 var connection;
 
-	// DB is local on localhost
+if (process.env.JAWSDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+         // DB is local on localhost
 	connection = mysql.createConnection({
 		port: 3306,
 		host: 'localhost',
